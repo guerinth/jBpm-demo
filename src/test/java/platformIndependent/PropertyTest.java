ccbb7d61c09d3147341b9373c4b5eee49ed95241
Guerin <t.guerin@groupeonepoint.com>
2018-06-21 16:19:54 +0200

    Ajout lecture variable environnement

--- src/test/java/platformIndependent/PropertyTest.java
+++ src/test/java/platformIndependent/PropertyTest.java
@@ -1,27 +1,28 @@
-package jndi;
+package platformIndependent;
 
 import javax.naming.NamingException;
 
+import org.jbpm.bpmn2.handler.SendTaskHandler;
 import org.jbpm.test.JbpmJUnitBaseTestCase;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkItemHandler;
 
-public class JndiTest extends JbpmJUnitBaseTestCase {
+public class PropertyTest extends JbpmJUnitBaseTestCase {
 
-	public JndiTest() {
+	public PropertyTest() {
 		// don't setup data source and enable persistence
 		super(false, false);
 	}
 
 	@Test
-	@Ignore
+	// @Ignore
 	public void testGetUrl() throws NamingException {
 
 		// create runtime manager with single process
-		createRuntimeManager("jndi/getUrlsFromJndi.bpmn2");
+		createRuntimeManager("platformIndependent/getUrls.bpmn2");
 
 		// take RuntimeManager to work with process engine
 		RuntimeEngine runtimeEngine = getRuntimeEngine();
@@ -29,10 +30,14 @@ public class JndiTest extends JbpmJUnitBaseTestCase {
 		// get access to KieSession instance
 		KieSession ksession = runtimeEngine.getKieSession();
 
+		// Register a test handler for "Rest"
+		WorkItemHandler testHandler = new SendTaskHandler();
+		ksession.getWorkItemManager().registerWorkItemHandler("Rest", testHandler);
+
 		// start process
-		ProcessInstance processInstance = ksession.startProcess("jndi.getUrlsFromJndi");
+		ProcessInstance processInstance = ksession.startProcess("platformIndependent.getUrls");
 
 		// check what nodes have been triggered
-		assertNodeTriggered(processInstance.getId(), "Valeur JNDI lue");
+		assertNodeTriggered(processInstance.getId(), "Demande d'appel", "Appel REST", "Appel REST réalisé");
 	}
 }
